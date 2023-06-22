eeca8a19ab66b6acfc12e5d9005f1f7076a95e43
MD SHARIK <135040117+GeekCoder55@users.noreply.github.com>
2023-06-22 09:18:18 +0000

    data types

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+("use strict")
+let name = "sharik"
+let wifi = null
+let age = 255
+let alive = true
+
+// primitive data type
+
+// string = "string"
+// number = 15
+// boolean = true or false'
+// symbol = define uniqueness
+// null = define empty or null
+// undefined = undefined
+
+// type of = define the type of datatypes or variables
+
+console.table( [typeof null, typeof name, typeof age, typeof wifi])
--- 01_Basics/learn.js
+++ 01_Basics/learn.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+const accountID = 54154
+let accountEmail = "md.sharik5012@gmail.com"
+var accountPassword = "123456"
+comment = "Coding is life"
+let accountState;
+/* this is comment: in java script without assinging variable
+ like Let or var a variable can be assigned likr in comment
+ but it is not a prefered always use variable syntax
+*/
+
+
+/*
+ Prefer not to use var :
+ because it creates issues for block Scopes and functional scopes!
+
+*/
+
+
+accountEmail  = "gg!"
+accountPassword = "get a life!"
+
+console.log(accountPassword);
+console.log(accountEmail);
+
+console.table([accountEmail, accountID, accountPassword, comment, accountState])
