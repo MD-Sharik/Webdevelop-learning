b10254a2564ccf42ed7a083dc462fef331fb43bb
MD SHARIK <135040117+MD-Sharik@users.noreply.github.com>
2023-06-25 21:05:57 +0000

    Date and Time

---
+++ 01_Basics/Date_and_time.js
@@ -0,0 +1,8 @@
+const myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString('en-IN'));
+console.log(typeof myDate);
